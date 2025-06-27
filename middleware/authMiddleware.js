db2dfd7fbf1748fe15acc9cb424d07fa91fd4672
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-27 19:25:52 +0530

    Resolved token error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
   // Check if user is authenticated
-  const token = req.headers;
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     res.status(401).json({
